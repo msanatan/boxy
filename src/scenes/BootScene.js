ba29d421c483aa9fe46f20aca6f158ab9af02b01
Marcus Sanatan <msanatan@gmail.com>
2018-05-18 01:18:39 -0400

    Load tilemap and tileset in game

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,5 +1,7 @@
 import { Scene } from 'phaser'
 import logo from '../assets/logo.png'
+import colour_palette from '../assets/tileset/colour_palette.png'
+import level1 from '../assets/tilemaps/level1.json'
 
 export default class BootScene extends Scene {
   constructor () {
@@ -8,6 +10,8 @@ export default class BootScene extends Scene {
 
   preload () {
     this.load.image('logo', logo)
+    this.load.image('colour_palette', colour_palette);
+	  this.load.tilemapTiledJSON('level1', level1);
   }
 
   create () {
