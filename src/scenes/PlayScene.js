7a423f77f4af435ddad9e5a145c2433ff2379035
Marcus Sanatan <marcus.sanatan@wepala.com>
2018-05-21 01:42:50 -0400

    Player dies if falls in lava After trying to get collisions and boundaries to work, I use the tile index callback to determine if the player falls in lava and kills them. The player dying simply resets their position and does a small animation as with most easy going platformers

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -12,25 +12,38 @@ export default class PlayScene extends Scene {
     let backgroundLayer = this.map.createStaticLayer('Background', tiles);
     this.platformLayer = this.map.createDynamicLayer('Platforms', tiles);
     this.platformLayer.setCollisionByExclusion([-1]);
-    this.lavaLayer = this.map.createDynamicLayer('Lava', tiles);
+    this.originalPlayerX = this.map.tileWidth;
+    this.originalPlayerY = this.map.heightInPixels - (this.map.tileHeight * 2);
 
     // Create the player
     this.player = this.physics.add.sprite(
-      this.map.tileWidth,
-      this.map.heightInPixels - (this.map.tileHeight * 2),
+      this.originalPlayerX,
+      this.originalPlayerY,
       'player');
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
+    this.lavaLayer = this.map.createDynamicLayer('Lava', tiles);
+    this.lavaLayer.setTileIndexCallback(2, () => this.playerDies());
 
     // We need to scale the world in the physics engine
     this.physics.world.bounds.width = this.map.widthInPixels;
     this.physics.world.bounds.height = this.map.heightInPixels;
     this.physics.add.collider(this.player, this.platformLayer);
+    this.physics.add.overlap(this.player, this.lavaLayer);
     // Set the bounds on the camera as well
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
     this.cameras.main.startFollow(this.player);
 
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.playerDieTween = {
+      targets: this.player,
+      alpha: 1,
+      duration: 100,
+      ease: 'Linear',
+      repeat: 10,
+    };
+
   }
 
   update(time, delta) {
@@ -46,5 +59,19 @@ export default class PlayScene extends Scene {
     if ((this.cursors.space.isDown) && this.player.body.onFloor()) {
       this.player.body.setVelocityY(-350);
     }
+  }
+
+  checkContains(sprite1, sprite2) {
+    let bounds1 = sprite1.getBounds();
+    let bounds2 = sprite2.getBounds();
+    return Phaser.Geom.Rectangle.Contains(bounds1, bounds2);
+  }
+
+  playerDies() {
+    this.player.setVelocity(0, 0);
+    this.player.x = this.originalPlayerX;
+    this.player.y = this.originalPlayerY;
+    this.player.setAlpha(0);
+    let tw = this.tweens.add(this.playerDieTween);
   }
 }
