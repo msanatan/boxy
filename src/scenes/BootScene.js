e437c84a1596eba670432d2855757174e375ba04
Marcus Sanatan <marcus.sanatan@wepala.com>
2018-06-03 17:15:57 -0400

    Load the audio during boot To make this work I had to modify the phaser cli scripts to include mp3 and ogg extensions in the file loader. I'll make a pull request soon to make it permanent

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -2,6 +2,8 @@ import { Scene } from 'phaser'
 import colourPalette from '../assets/tileset/colour_palette.png'
 import level1 from '../assets/tilemaps/level1.json'
 import player from '../assets/player.png'
+import playerDieSfxOgg from '../assets/audio/sfx_sounds_negative1.ogg'
+import playerDieSfxMp3 from '../assets/audio/sfx_sounds_negative1.mp3'
 
 export default class BootScene extends Scene {
   constructor () {
@@ -12,6 +14,7 @@ export default class BootScene extends Scene {
     this.load.image('player', player);
     this.load.spritesheet('colourPalette', colourPalette, {frameWidth: 35, frameHeight: 35});
     this.load.tilemapTiledJSON('level1', level1);
+    this.load.audio('playerDiesSfx', [playerDieSfxOgg, playerDieSfxMp3]);
   }
 
   create () {
